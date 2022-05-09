64c6f5aa817919a0c434e7c5c873b6d98afe6c1c
Naveen <naveen.ramkumar@zoominfo.com>
2022-05-09 15:12:32 +0530

    adding recommendations

--- src/app/recommendationpanel/recommendationpanel.component.ts
+++ src/app/recommendationpanel/recommendationpanel.component.ts
@@ -11,7 +11,8 @@ export interface Recommendation {
   buttonAction: string,
   points: string,
   extra: string,
-  imageName: string
+  imageName: string,
+  reco: number
 }
 
 export interface Leaderboards {
